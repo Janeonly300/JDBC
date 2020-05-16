fd5f4147e2b8c3cf60c0a9f99a6d1a16a87634ce
jianyi <1261794911@qq.com>
2020-05-16 23:18:03 +0800

    新增多条表数据查询

--- com/jianyi/prepraedStatementQuery/PreparedStatementQueryTest.java
+++ com/jianyi/prepraedStatementQuery/PreparedStatementQueryTest.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,6 +20,67 @@ import java.sql.ResultSetMetaData;
  * @crateTime 2020-05-16 03:48
  */
 public class PreparedStatementQueryTest {
+
+    @Test
+    public void test2(){
+        String sql = "select id,name ,email,birth from customer";
+        List<Customer> customers = QueryOOP(Customer.class, sql);
+        customers.forEach(System.out::println);
+    }
+
+
+    /**
+     *
+     * 通用查询，支持多条记录查询
+     */
+    public<T> List<T> QueryOOP(Class<T> clazz, String sql , Object...args){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<T> list = null;
+        try {
+            //获取数据库连接对象
+            conn = JDBCUtil.getConnection();
+            //获取数据库操作对象
+            ps = conn.prepareStatement(sql);
+            //填充占位符
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i+1,args[i]);
+            }
+
+            //获取查询结果集
+            rs = ps.executeQuery();
+            //获取结果集元数据
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取字段数
+            int columnCount = rsmd.getColumnCount();
+
+            //创建集合
+            list = new ArrayList<>();
+            while(rs.next()){
+                T t = clazz.newInstance();
+                for( int i =0;i<columnCount;i++){
+                    //获取字段值
+                    Object value = rs.getObject(i + 1);
+                    //获取字段别名
+                    String columnLabel = rsmd.getColumnLabel(i+1);
+
+                    //属性赋值
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t,value);
+                }
+                list.add(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.closeResource(conn,ps,rs);
+        }
+        return list;
+
+    }
+
 
     @Test
     public void test1(){
